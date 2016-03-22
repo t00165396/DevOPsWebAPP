e5f721e4e7346142ae975497062a52d085b3af98
t00165396 <liranda.krasniqi@students.ittralee.ie>
2016-03-21 20:43:34 -0700

    .

---
+++ DevOPsWebAPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOPsWebAPP.Startup))]
+namespace DevOPsWebAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
