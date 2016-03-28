93f32d5ede43b33cf13e54b57bd99b8d85a1c81a
t00165396 <liranda.krasniqi@students.ittralee.ie>
2016-03-28 01:59:10 -0700

    added nunit tests

---
+++ WebbAPPNunit/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbAPPNunit
+{
+
+   
+    public class Class1
+    {
+        [Test]
+        public void index() {
+
+            Assert.True(true);
+
+        }
+
+    }
+}
