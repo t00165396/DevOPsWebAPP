75a7605766f189dbe243f0f54ea684c3107442c9
t00165396 <liranda.krasniqi@students.ittralee.ie>
2016-04-25 17:57:02 -0700

    Unit tests updates

---
+++ RecipeCalculatorTests/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RecipeCalculator;
+
+namespace RecipeCalculatorTests
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        const double DEFAULT_UNIT_SIZE = 1.0;
+        [Test]
+
+        public void AddServingShouldIncreaseUnitSize()
+        {
+            //Arrange
+            Ingredient control = new Garlic(null, DEFAULT_UNIT_SIZE);
+            Ingredient recipe = new Garlic(null, DEFAULT_UNIT_SIZE);
+
+            //Assert
+            Assert.IsTrue(control.Units == DEFAULT_UNIT_SIZE);
+            Assert.IsTrue(recipe.Units == DEFAULT_UNIT_SIZE);
+
+            //Act
+            recipe.addServing(DEFAULT_UNIT_SIZE);
+
+            //Assert
+            Assert.IsTrue(recipe.Units > control.Units);
+            Assert.IsTrue(recipe.Units == (DEFAULT_UNIT_SIZE * 2));
+        }
+    }
+}
